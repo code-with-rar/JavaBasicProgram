9dcc19a60c726fdc5601c5f90093541c09b7f1c8
USER <USER@DESKTOP-IDU24V8>
2023-08-13 22:02:38 +0530

    Add rahul elect class

--- src/day1/Abc.java
+++ src/day1/Abc.java
@@ -1,5 +1,8 @@
 package day1;
 
 public class Abc {
+	public static void main(String[] args) {
+		System.out.println("rahul_Electronics");
+	}
 
 }
---
+++ src/day1/Rahul_electronics.java
@@ -0,0 +1,5 @@
+package day1;
+
+public class Rahul_electronics {
+
+}
